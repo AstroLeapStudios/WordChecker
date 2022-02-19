c16e3c5edab8a58c4fe92fcafcb78160b8fe2ff7
Cole Timlin <95893912+AstroLeapStudios@users.noreply.github.com>
2022-02-19 17:38:59 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
+from operator import truediv
 import os
 from termcolor import cprint
 from pyfiglet import figlet_format
-from colorama import init
 
 os.system("title Word Checker")
 cprint(figlet_format('WrdChckr', font='big'),
        'blue', attrs=['bold'])
 print("Add the .txt file to the directory of this code.\n ")
 
-fileName = input("Name of the .txt file (excluding the file extension): ")
+while True:
+       fileName = input("Name of the .txt file (excluding the file extension): ")
 
-file = open(f"{fileName}.txt",encoding='latin-1')
-full_text = file.read()
+       file = open(f"{fileName}.txt",encoding='latin-1')
+       full_text = file.read()
 
-print(f"\nFile ({fileName}.txt) contains {len(full_text)} characters. \n")
+       print(f"\nFile ({fileName}.txt) contains {len(full_text)} characters. \n")
 
-num = input("How many words would you like to check for?: ")
-print("\n")
-n = range(int(num))
+       num = input("How many words would you like to check for?: ")
+       print("\n")
+       n = range(int(num))
 
-for i in n:
-       word = input(f"Word {i + 1}: ")
-       count = 0 
-       l = full_text.split(" ")
-       for c in l:
-              if(c.upper() == word.upper()):
-                     count += 1
-       print(f"{fileName}.txt includes the word {word} {count} times \n")
+       for i in n:
+              word = input(f"Word {i + 1}: ")
+              count = 0 
+              l = full_text.split(" ")
+              for c in l:
+                     if(c.upper() == word.upper()):
+                            count += 1
+              print(f"{fileName}.txt includes the word {word} {count} times \n")
