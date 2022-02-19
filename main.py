a3b3edd93dfdfd1a6296d67765c729472697d04d
Cole Timlin <95893912+AstroLeapStudios@users.noreply.github.com>
2022-02-19 12:17:13 -0500

    Main Push

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+from termcolor import cprint
+from pyfiglet import figlet_format
+from colorama import init
+
+os.system("title Word Checker")
+cprint(figlet_format('WrdChckr', font='big'),
+       'blue', attrs=['bold'])
+print("Add the .txt file to the directory of this code.\n ")
+
+fileName = input("Name of the .txt file (excluding the file extension): ")
+
+file = open(f"{fileName}.txt",encoding='latin-1')
+full_text = file.read()
+
+print(f"\nFile ({fileName}.txt) contains {len(full_text)} characters. \n")
+
+num = input("How many words would you like to check for?: ")
+print("\n")
+n = range(int(num))
+
+for i in n:
+       word = input(f"Word {i + 1}: ")
+       count = 0 
+       l = full_text.split(" ")
+       for c in l:
+              if(c.upper() == word.upper()):
+                     count += 1
+       print(f"{fileName}.txt includes the word {word} {count} times \n")
